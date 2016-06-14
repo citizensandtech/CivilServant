6e8767ba24b4935a6c2ed384ea6ad89901a8659d
Nathan Matias <natematias@gmail.com>
2016-06-14 11:02:08 -0400

    Added support for per-controller reddit access tokens, managed centrally through praw-multiprocess and stored in MySQL

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 # example taken from http://pythoncentral.io/introductory-tutorial-python-sqlalchemy/
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime, String
 from sqlalchemy.dialects.mysql import MEDIUMTEXT
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 import sqlalchemy
 import datetime
+import socket
 
 Base = declarative_base()
 
@@ -16,3 +17,24 @@ class FrontPage(Base):
     id = Column(Integer, primary_key = True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     page_data = Column(MEDIUMTEXT)
+
+
+class PrawKey(Base):
+    __tablename__ = 'praw_keys'
+    # IDs will be a string based on the assumption
+    # that each device will only have one process
+    # at a time handling a particular controller
+    # in the format:  
+    #   HOST:ENV:CONTROLLER
+    # For example:
+    #   hannahmore:development:FrontPageController
+    id = Column(String(256), primary_key = True)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    access_token = Column(String(256))
+    scope = Column(String(256)) #json
+    refresh_token = Column(String(256))
+
+    @classmethod
+    def get_praw_id(cls, env, controller):
+        host = socket.gethostname()
+        return "{0}:{1}:{2}".format(host,env,controller)
--- reddit/connection.py
+++ reddit/connection.py
@@ -1,11 +1,80 @@
 import praw
+from praw.handlers import MultiprocessHandler
 import pickle
-import os
+import os, inspect
+from app.models import PrawKey
+import simplejson as json
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base
+import sqlalchemy
 
-def connect():
-    env =  os.environ['CS_ENV']
-    r = praw.Reddit(user_agent="Test version of CivilServant by u/natematias")
-    with open("config/access_information_{environment}.pickle".format(environment=env), "rb") as fp:
-        access_information = pickle.load(fp)
-        r.set_access_credentials(**access_information)
+ENV =  os.environ['CS_ENV']
+
+class Connect:
+
+  # this initializer accepts a database session
+  # and if it doesn't exist, initializes one
+  # this may become a pattern that we should break out
+  # into its own class eventually
+  def __init__(self, db_session = None):
+    ## LOAD DATABASE OBJECT
+    with open("config/{env}.json".format(env=ENV), "r") as config:
+      DBCONFIG = json.loads(config.read())
+    
+    if db_session is None:
+      ### LOAD SQLALCHEMY SESSION
+      db_engine = create_engine("mysql://{user}:{password}@{host}/{database}".format(
+          host = DBCONFIG['host'],
+          user = DBCONFIG['user'],
+          password = DBCONFIG['password'],
+          database = DBCONFIG['database']))
+
+      Base.metadata.bind = db_engine
+      DBSession = sessionmaker(bind=db_engine)
+
+      # set database session
+      self.db_session = DBSession()
+    else:
+      self.db_session = db_session
+    
+  def connect(self, controller="Main"):
+    r = None #Praw Connection Object
+    handler = MultiprocessHandler()
+
+    # Check the Database for a Stored Praw Key
+    db_praw_id = PrawKey.get_praw_id(ENV, controller)
+    pk = self.db_session.query(PrawKey).filter_by(id=db_praw_id).first()
+    r = praw.Reddit(user_agent="Test version of CivilServant by u/natematias", handler=handler)
+    
+    access_information = {}
+    
+    if(pk is None):
+      with open("config/access_information_{environment}.pickle".format(environment=ENV), "rb") as fp:
+          access_information = pickle.load(fp)
+    else:
+      access_information['access_token'] = pk.access_token
+      access_information['refresh_token'] = pk.refresh_token
+      access_information['scope'] = json.loads(pk.scope)
+    
+    r.set_access_credentials(**access_information)
+
+    new_access_information = {}
+    update_praw_key = False
+
+    # SAVE OR UPDATE THE ACCESS TOKEN IF NECESSARY
+    if(pk is None):
+      pk = PrawKey(id=db_praw_id,
+                   access_token = r.access_token,
+                   refresh_token = r.refresh_token,
+                   scope = json.dumps(list(r._authentication)))
+      self.db_session.add(pk)
+
+    elif(access_information['access_token'] != r.access_token or
+       access_information['refresh_token'] != r.refresh_token):
+       pk.access_token = r.access_token
+       pk.refresh_token = r.refresh_token
+       pk.scope = json.dumps(list(r._authentication))
+      
+    self.db_session.commit()
     return r
