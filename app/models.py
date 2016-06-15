eb8128221425899f5dfef2cff6e5001aba2ee90b
Nathan Matias <natematias@gmail.com>
2016-06-15 10:27:52 -0400

    edited alembic migrations and pluralized table names

--- app/models.py
+++ app/models.py
@@ -12,27 +12,27 @@ import datetime
 Base = declarative_base()
 
 class FrontPage(Base):
-    __tablename__ = 'front_page'
+    __tablename__ = 'front_pages'
     id = Column(Integer, primary_key = True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     page_type = Column(Integer) # see utils/common.py
     page_data = Column(MEDIUMTEXT)
 
 class Subreddit(Base):
-    __tablename__ = 'subreddit'
+    __tablename__ = 'subreddits'
     id = Column(String(32), primary_key = True, unique=True, autoincrement=False) # reddit's id
     name = Column(String(32)) # display_name
 
 class SubredditPage(Base):
-    __tablename__ = 'subreddit_page'
+    __tablename__ = 'subreddit_pages'
     id = Column(Integer, primary_key = True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
     page_type = Column(Integer) # see utils/common.py
     page_data = Column(MEDIUMTEXT)
 
 class Post(Base):
-    __tablename__ = 'post'
+    __tablename__ = 'posts'
     id = Column(String(32), primary_key = True, unique=True, autoincrement=False)	# reddit's id "id"
-    subreddit_id = Column(String(32), ForeignKey('subreddit.id'))	# "subreddit_id"
+    subreddit_id = Column(String(32), ForeignKey('subreddits.id'))	# "subreddit_id"
     created = Column(DateTime) # "created"
     post_data = Column(MEDIUMTEXT)	# "json_dict"
