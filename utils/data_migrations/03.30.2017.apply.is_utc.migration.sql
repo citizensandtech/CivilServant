7ee6aafb039ceeb516e3eda657e1efaed4aab12b
J. Nathan Matias <natematias@gmail.com>
2017-03-31 10:34:15 -0400

    optimized queries in is_utc migration

--- utils/data_migrations/03.30.2017.apply.is_utc.migration.sql
+++ utils/data_migrations/03.30.2017.apply.is_utc.migration.sql
@@ -1,12 +1,12 @@
 /* RUN THIS FIRST */
 DROP TABLE subreddit_page_utc_ids
 DROP TABLE front_page_utc_ids
-CREATE TABLE subreddit_page_utc_ids (id INTEGER, is_utc INTEGER);
-CREATE TABLE front_page_utc_ids (id INTEGER, is_utc INTEGER);
+CREATE TABLE subreddit_page_utc_ids (id INTEGER);
+CREATE TABLE front_page_utc_ids (id INTEGER);
 
 /* THEN RUN THE SCRIPT*/
 
 /* THEN RUN THESE QUERIES */
 
-UPDATE front_pages SET is_utc=NULL WHERE id in (select id from front_page_utc_ids);
-UPDATE subreddit_pages SET is_utc=NULL WHERE id in (select id from subreddit_page_utc_ids);
+UPDATE front_pages fp RIGHT JOIN front_page_utc_ids ids ON (fp.id = ids.id) SET fp.is_utc=NULL;
+UPDATE subreddit_pages sp RIGHT JOIN subreddit_page_utc_ids ids ON (sp.id = ids.id) SET sp.is_utc=NULL;
