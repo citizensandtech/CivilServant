ac602dd21a2bc20e0c2b69a1dac3ccdf941e0088
Jonathan Zong <jonathan@jonathanzong.com>
2018-02-17 12:12:54 -0500

    consistent LumenNotice job state in case of exception

--- app/controllers/lumen_controller.py
+++ app/controllers/lumen_controller.py
@@ -89,17 +89,7 @@ class LumenController():
     """
     def query_and_parse_notices_archive_users(self, test_exception = False):
         unparsed_notices = self.db_session.query(LumenNotice).filter(LumenNotice.CS_parsed_usernames == CS_JobState.NOT_PROCESSED.value).all()
-
-        utils.common.update_CS_JobState(unparsed_notices, "CS_parsed_usernames", CS_JobState.IN_PROGRESS, self.db_session, self.log)
-
-        if(test_exception):
-            raise Exception("Throwing an exception for test purposes")
-
-
-        notice_to_state = self.parse_notices_archive_users(unparsed_notices)
-
-        utils.common.update_all_CS_JobState(notice_to_state, "CS_parsed_usernames", self.db_session, self.log)
-
+        notice_to_state = self.parse_notices_archive_users(unparsed_notices, test_exception)
 
     """
         unparsed_notices = list of LumenNotices
@@ -107,7 +97,7 @@ class LumenController():
         returns:
             notice_to_state = {LumenNotice: CS_JobState}
     """
-    def parse_notices_archive_users(self, unparsed_notices):
+    def parse_notices_archive_users(self, unparsed_notices, test_exception = False):
         if len(unparsed_notices) == 0:
             return {}
 
@@ -115,78 +105,104 @@ class LumenController():
         if not is_test: # to accomodate test fixture data
             notice_to_state = {notice: CS_JobState.FAILED for notice in unparsed_notices }
         else:
-            notice_to_state = {json.dumps(notice): CS_JobState.FAILED for notice in unparsed_notices } 
+            notice_to_state = {json.dumps(notice): CS_JobState.FAILED for notice in unparsed_notices }
+
+        if(test_exception):
+            counter = 0
 
         for notice in unparsed_notices:
-            notice_json = json.loads(notice.notice_data) if not is_test else notice # to accomodate test fixture data
-            notice_users = set([])
-            suspended_user_count = 0
-            job_state = None
-            for work in notice_json["works"]:
-                # infringing_urls is known to contain urls
-                for url_obj in work["infringing_urls"]:
-                    url = url_obj["url"]
-                    try:
-                        username = helper_parse_url_for_username(url, self.log)
-                    except utils.common.ParseUsernameSuspendedUserFound:
-                        suspended_user_count += 1
-                    except Exception as e:
-                        self.log.error("Unexpected error while calling helper_parse_url_for_username on url {0}: {1}".format(url, e))
-                    else:
-                        if username:
-                            # if no username, then no username found
-                            notice_users.add(username)
-
-                if len(work["copyrighted_urls"]) > 0:  # I've only seen this empty
-                    self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['works']['copyrighted_urls']; notice id = {0}".format(notice_json["id"]))                
-                    job_state = CS_JobState.NEEDS_RETRY
-                if work["description"]:  # I've only seen this null
-                    self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['works']['description']; notice id = {0}".format(notice_json["id"]))                
-                    job_state = CS_JobState.NEEDS_RETRY
-            if notice_json["body"]:  # I've only seen this null
-                self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['body']; notice id = {0}".format(notice_json["id"]))                
-                job_state = CS_JobState.NEEDS_RETRY
-
-            now = datetime.datetime.utcnow()
-            # for every notice, commit LumenNoticeToTwitterUser records 
-            for username in notice_users:
-                notice_user_record = LumenNoticeToTwitterUser(
-                        record_created_at = now,
-                        notice_id = notice_json["id"],
-                        twitter_username = username.lower(),
-                        twitter_user_id = None,
-                        CS_account_archived = CS_JobState.NOT_PROCESSED.value
-                    )
-                self.db_session.add(notice_user_record)
-
-            # this notice has suspended_user_count not found users (t.co URL redirected to an account/suspended page)
-            for i in range(suspended_user_count):
-                notice_user_record = LumenNoticeToTwitterUser(
-                        record_created_at = now,
-                        notice_id = notice_json["id"],
-                        twitter_username = utils.common.NOT_FOUND_TWITTER_USER_STR,
-                        twitter_user_id = utils.common.NOT_FOUND_TWITTER_USER_STR,
-                        CS_account_archived = CS_JobState.PROCESSED.value # can't do anything about these records. don't process
-                    )
-                self.db_session.add(notice_user_record)                
+            notice_old_job_state = notice.CS_parsed_usernames
+            notice.CS_parsed_usernames = CS_JobState.IN_PROGRESS.value
+            self.db_session.add(notice)
+            self.db_session.commit()
 
             try:
-                self.db_session.commit()
-            except:
-                self.log.error("Error while saving {0} twitter users from {1} infringing_urls in notice {2} DB Session".format(
-                    len(notice_users),
-                    sum(len(work["infringing_urls"]) for work in notice_json["works"]),
-                    notice_json["id"]), extra=sys.exc_info()[0])
-                ####return notice_to_state
-            else:
-                self.log.info("Saved {0} twitter users from {1} infringing_urls in notice {2}.".format(
-                    len(notice_users),
-                    sum(len(work["infringing_urls"]) for work in notice_json["works"]),
-                    notice_json["id"]))
+                notice_json = json.loads(notice.notice_data) if not is_test else notice # to accomodate test fixture data
+                notice_users = set([])
+                suspended_user_count = 0
+                job_state = None
+                for work in notice_json["works"]:
+                    # infringing_urls is known to contain urls
+                    for url_obj in work["infringing_urls"]:
+                        url = url_obj["url"]
+                        try:
+                            username = helper_parse_url_for_username(url, self.log)
+                        except utils.common.ParseUsernameSuspendedUserFound:
+                            suspended_user_count += 1
+                        except Exception as e:
+                            self.log.error("Unexpected error while calling helper_parse_url_for_username on url {0}: {1}".format(url, e))
+                        else:
+                            if username:
+                                # if no username, then no username found
+                                notice_users.add(username)
 
-                job_state = CS_JobState.PROCESSED if (job_state is not CS_JobState.NEEDS_RETRY) else CS_JobState.NEEDS_RETRY
-                key = notice if not is_test else json.dumps(notice)
-                notice_to_state[key] = job_state
+                    if len(work["copyrighted_urls"]) > 0:  # I've only seen this empty
+                        self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['works']['copyrighted_urls']; notice id = {0}".format(notice_json["id"]))
+                        job_state = CS_JobState.NEEDS_RETRY
+                    if work["description"]:  # I've only seen this null
+                        self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['works']['description']; notice id = {0}".format(notice_json["id"]))
+                        job_state = CS_JobState.NEEDS_RETRY
+                if notice_json["body"]:  # I've only seen this null
+                    self.log.error("method helper_parse_notices_archive_users: maybe missed something in notice_json['body']; notice id = {0}".format(notice_json["id"]))
+                    job_state = CS_JobState.NEEDS_RETRY
+
+                now = datetime.datetime.utcnow()
+                # for every notice, commit LumenNoticeToTwitterUser records
+                for username in notice_users:
+                    notice_user_record = LumenNoticeToTwitterUser(
+                            record_created_at = now,
+                            notice_id = notice_json["id"],
+                            twitter_username = username.lower(),
+                            twitter_user_id = None,
+                            CS_account_archived = CS_JobState.NOT_PROCESSED.value
+                        )
+                    self.db_session.add(notice_user_record)
+
+                # this notice has suspended_user_count not found users (t.co URL redirected to an account/suspended page)
+                for i in range(suspended_user_count):
+                    notice_user_record = LumenNoticeToTwitterUser(
+                            record_created_at = now,
+                            notice_id = notice_json["id"],
+                            twitter_username = utils.common.NOT_FOUND_TWITTER_USER_STR,
+                            twitter_user_id = utils.common.NOT_FOUND_TWITTER_USER_STR,
+                            CS_account_archived = CS_JobState.PROCESSED.value # can't do anything about these records. don't process
+                        )
+                    self.db_session.add(notice_user_record)
+
+                if(test_exception):
+                    counter += 1
+                    if counter >= len(unparsed_notices) / 2:
+                        raise Exception("Throwing an exception for test purposes")
+
+            except:
+                # something unhandled went wrong during processing
+                notice.CS_parsed_usernames = notice_old_job_state
+                self.db_session.add(notice)
+                self.db_session.commit()
+                raise # re-raise exception
+            else:
+                # commit previously added LumenNoticeToTwitterUser record
+                # add & commit LumenNotice job state
+                # finish processing
+                try:
+                    job_state = CS_JobState.PROCESSED if (job_state is not CS_JobState.NEEDS_RETRY) else CS_JobState.NEEDS_RETRY
+                    notice.CS_parsed_usernames = job_state.value
+                    self.db_session.add(notice)
+                    self.db_session.commit()
+                except:
+                    self.log.error("Error while saving {0} twitter users from {1} infringing_urls in notice {2} DB Session".format(
+                        len(notice_users),
+                        sum(len(work["infringing_urls"]) for work in notice_json["works"]),
+                        notice_json["id"]), extra=sys.exc_info()[0])
+                    ####return notice_to_state
+                else:
+                    self.log.info("Saved {0} twitter users from {1} infringing_urls in notice {2}.".format(
+                        len(notice_users),
+                        sum(len(work["infringing_urls"]) for work in notice_json["works"]),
+                        notice_json["id"]))
+
+                    key = notice if not is_test else json.dumps(notice)
+                    notice_to_state[key] = job_state
 
         return notice_to_state
 
@@ -301,8 +317,8 @@ class LumenController():
                     return url_dict
 
 
-# assume url is of the form 'https://twitter.com/sooos243/status/852942353321140224' 
-# OR check if a t.co url extends to a twitter.com url 
+# assume url is of the form 'https://twitter.com/sooos243/status/852942353321140224'
+# OR check if a t.co url extends to a twitter.com url
 # interesting later study: see how many t.co links resolve to twitter links?
 def helper_parse_url_for_username(url, log):
     twitter_domain = "twitter.com"
@@ -311,7 +327,7 @@ def helper_parse_url_for_username(url, log):
     url_split = url.split("/")
     retries = 3
 
-    # TODO: how to resolve t.co urls without hitting twitter.com without auth tokens (since we're getting rate limited?) 
+    # TODO: how to resolve t.co urls without hitting twitter.com without auth tokens (since we're getting rate limited?)
     # calling requests.get is very time inefficient
     if len(url_split) >= 3 and url_split[2] == tco_domain:
         pass
@@ -330,7 +346,7 @@ def helper_parse_url_for_username(url, log):
         #####    raise Exception
 
     if url == "https://twitter.com/account/suspended":
-        # TODO: then we have no information. 
+        # TODO: then we have no information.
         # save a LumenNoticeToTwitterUser record, with username = "SUSPENDED"
         raise utils.common.ParseUsernameSuspendedUserFound
 
