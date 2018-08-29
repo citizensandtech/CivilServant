eb17ab172bb56a08f2bf02dbc45675d87d44304e
Max Klein <isalix@gmail.com>
2018-08-28 17:33:51 -0700

    make script run from absolute

--- utils/email-reports.sh
+++ utils/email-reports.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
-if [ -z $CS_ENV ]
+if [ -z $1 ]
     then
-    echo "trying to source environment variables."
-    source ../config/environment_variables.sh
-    if [ -z $CS_ENV ]
-        then
-        echo "couldn't get CS_ENV out of the script, exting"
-        exit 1
-        else
-        echo "Found CS_ENV from script"
-    fi
+    envpython="/home/dmca/dmca/dmca/bin/python"
+    echo "Using default python of"$envpython
+    else
+    envpython=$1
+fi
+
+
+if [ -z $2 ]
+    then
+    basedir="/home/dmca/CivilServant-mmou-twitter"
+    echo "trying to source environment variables from"$basedir
+    else
+    basedir=$2
+fi
+
+if [ -z $3 ]
+    then
+    export CS_ENV=production
+    else
+    export CS_ENV=$3
 fi
 
 echo "Running with CS_ENV=$CS_ENV"
-
-if [ $CS_ENV = "development" ]
-    then
-    envpython=$1
-    basedir=$2
-    else if [ $CS_ENV = "production" ]
-    then
-    envpython="/home/dmca/dmca/dmca/bin/python"
-    basedir="/home/dmca/CivilServant-mmou-twitter/utils"
-    fi
-fi
 
 $envpython $basedir"/email_db_report.py"
 $envpython $basedir"/email_log_report.py"
