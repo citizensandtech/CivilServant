fc56ac4c0cd10d1cd1e177a901d434f7225635da
Max Klein <isalix@gmail.com>
2018-09-05 16:42:19 -0700

    don't ever stop logging, not even after 20mb

--- app/cs_logger.py
+++ app/cs_logger.py
@@ -15,7 +15,7 @@ def get_logger(ENV, BASE_DIR):
 
   # print all debug and higher to STDOUT
   # if the environment is development
-  if(ENV=="development"): 
+  if(ENV=="development"):
     stdoutHandler = logging.StreamHandler(sys.stdout)
     stdoutHandler.setLevel(logging.DEBUG)
     log.addHandler(stdoutHandler)
@@ -24,7 +24,7 @@ def get_logger(ENV, BASE_DIR):
   print("Logging to " + BASE_DIR + "/logs/CivilServant_" + ENV + ".log")
   formatter = logging.Formatter('%(asctime)s - %(name)s({env}) - %(levelname)s - %(message)s'.format(env=ENV))
 
-  rotateHandler = ConcurrentRotatingFileHandler(logfile, "a", 32 * 1000 * 1024, 5)
+  rotateHandler = ConcurrentRotatingFileHandler(logfile, "a", 32 * 1000 * 1024)
   rotateHandler.setLevel(logging.DEBUG)
   rotateHandler.setFormatter(formatter)
   log.addHandler(rotateHandler)
