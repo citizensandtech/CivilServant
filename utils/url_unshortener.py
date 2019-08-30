f7bb89de49e793d31eb807909746690c7d970325
Max Klein <isalix@gmail.com>
2019-08-30 15:43:32 -0700

    new catch everything approach

--- utils/url_unshortener.py
+++ utils/url_unshortener.py
@@ -1,3 +1,4 @@
+import copy
 import inspect
 import os
 
@@ -71,6 +72,8 @@ def bulkUnshorten(urls, workers=20, REQUEST_TIMEOUT=5, HOPS_LIMIT=4):
 
     while True:
 
+        prev_urls = copy.deepcopy(urls)
+
         session = FuturesSession(max_workers=workers)
         futures = []
 
@@ -98,13 +101,6 @@ def bulkUnshorten(urls, workers=20, REQUEST_TIMEOUT=5, HOPS_LIMIT=4):
                 except requests.exceptions.InvalidSchema as e:
                     req_url = obj._exception.args[0].split('No connection adapters were found for \'')[1].split('\'')[0]
                     urls = setErrorOnUrls(urls, req_url, "InvalidSchema")
-                    continue
-                except UnicodeDecodeError as e:
-                    log.error('UnicodeDecodeError error: {} for obj:'.format(e.object))
-                    continue
-                except UnicodeError as e:
-                    log.error('Unicode error: {} for obj:'.format(e))
-                    log.error('Objs internal exception {}'.format(obj._exception))
                     continue
                 except Exception as e:
                     log.error("An unknown error {} for obj".format(e))
@@ -144,133 +140,19 @@ def bulkUnshorten(urls, workers=20, REQUEST_TIMEOUT=5, HOPS_LIMIT=4):
                 else:
                     urls = setErrorOnUrls(urls, result.url, err_msg=result.status_code, status_code=result.status_code)
             session.close()
+
+            # find the elements that didn't change at all during the loop because I hate futures.
+            unchanged = []
+            for url_dict in urls:
+                matching_dicts = [pud for pud in prev_urls if url_dict['original_url']==pud['original_url']]
+                for pud in matching_dicts:
+                    if pud['curr_url'] == url_dict['curr_url'] and url_dict['success'] is None:
+                        # if the curr_url has remained the same and not success
+                        log.debug('i think theres an uncaught error on {}'.format(url_dict['original_url']))
+                        unchanged.append(url_dict)
+
+            for unchange in unchanged:
+                urls = setErrorOnUrls(urls, unchange['curr_url'], 'UnresolveableError')
+
         else:
-            # unsuccessful_without_error = [url_dict for url_dict in urls if
-            #                              url_dict['orginal_url']==url_dict['curr_url'] and url_dict['success'] is None]
-            # for unsuccessful in unsuccessful_without_error:
-            #     urls = setErrorOnUrls(urls, unsuccessful, 'UnknownError')
             return urls
-
-
-# def bulkUnshortenLegacy(urls, workers=20):
-#     """
-#     Slightly more performant version, but will collapse output dimension based on a dict of the final URLs.
-#     This function will unshorten an array of shortened URLS
-#         The second optional argument is the number of workers to run in parallel
-#
-#         When initially called, an array of string objects will be passed to the function.
-#         The function will then create a dictionary to keep track of all urls, the number of hops and
-#         the final destination url.  If there is an error, a status code of 4xx is recorded within the dict.
-#         Otherwise, a status code of 200 should be returned.
-#
-#         Global timeouts
-#         - REQUEST_TIMEOUT is the timeout when waiting for a reply from a remote server
-#         - HOPS_LIMIT is the maximum number of redirect hops allowed
-#     """
-#     REQUEST_TIMEOUT = 10
-#     HOPS_LIMIT = 5
-#     log.debug(f"Bulk Unshorten called with {len(urls)} urls. Num workers specified: {workers}")
-#     log.debug(f"Hops limit is {HOPS_LIMIT} and request Timeout Seconds = {REQUEST_TIMEOUT}")
-#
-#     # Allow passing in of one url as a string object
-#     if (isinstance(urls, str)):
-#         urls = [urls]
-#
-#     # If method is being called initally, create a dictionary for the urls passed.  When the method calls
-#     # itself, it will pass this object to itself as needed.
-#     if (isinstance(urls, list)):
-#         url_objects = urls[:]
-#         urls = {}
-#         for url in url_objects:
-#             try:
-#                 req = requests.Request('HEAD', url)
-#                 normalized_url = req.prepare().url
-#                 urls[normalized_url] = {"hops": 0, "status_code": None, "success": None, "final_url": None,
-#                                         "error": None,
-#                                         "original_url": url}
-#             except InvalidURL as e:  # there are no guarantees in this world.
-#                 log.error(f"Error Unshortening: InvalidURL on {url}. Error {e}")
-#                 url['success'] = False
-#             except MissingSchema as e:
-#                 log.error(f"Error Unshortening: MissingSchema on {url}. Error {e}")
-#                 url['success'] = False
-#         log.debug(f'Starting URLS before unshortening are: {urls}')
-#
-#     while True:
-#         # print('there are {} urls in the middle'.format(len(urls)))
-#
-#         session = FuturesSession(max_workers=workers)
-#         futures = []
-#
-#         for key in urls:
-#             if urls[key]['success'] is not None: continue
-#             if urls[key]['hops'] >= HOPS_LIMIT: continue
-#             futures.append(session.head(key, timeout=REQUEST_TIMEOUT))
-#
-#         if futures:
-#             done, incomplete = wait(futures)
-#             for obj in done:
-#                 try:
-#                     result = obj.result()
-#                 except requests.exceptions.ConnectTimeout as e:
-#                     url = e.request.url
-#                     urls[url]['error'] = "ConnectTimeout"
-#                     urls[url]['success'] = False
-#                     continue
-#                 except requests.exceptions.ReadTimeout as e:
-#                     url = e.request.url
-#                     urls[url]['error'] = "ReadTimeout"
-#                     urls[url]['success'] = False
-#                     continue
-#                 except requests.exceptions.ConnectionError as e:
-#                     url = e.request.url
-#                     urls[url]['error'] = "ConnectionError"
-#                     urls[url]['success'] = False
-#                     continue
-#                 except UnicodeDecodeError as e:
-#                     log.error('Unicode error: {} for obj:'.format(e.object))
-#                     continue
-#
-#                 if result.status_code == 200:
-#                     urls[result.url]['success'] = True
-#                     urls[result.url]['final_url'] = result.url
-#                     urls[result.url]['status_code'] = result.status_code
-#                 elif result.status_code == 301 or result.status_code == 302:
-#                     try:
-#                         redirect_url = result.headers['location']
-#
-#                         # Handle a location header that returns a relative path instead of an absolute path.  This is now allowed
-#                         # under RFC 7231.  If the returned location does not begin with http, then it is a relative path and should
-#                         # be concatenated to the original url
-#
-#                         if not redirect_url.lower().startswith("http"):
-#                             redirect_url = result.url + redirect_url
-#
-#                         # Normalize the url using the requests module
-#                         req = requests.Request('HEAD', redirect_url)
-#                         redirect_url = req.prepare().url
-#
-#                         urls[result.url]['hops'] += 1
-#                         urls[result.url]['final_url'] = redirect_url
-#                         urls[result.url]['status_code'] = result.status_code
-#                         # Troublesome line that was collapsing the output dict by the URLS that had the same final destination
-#                         urls[redirect_url] = urls.pop(result.url)
-#                     except KeyError:  # no location to find
-#                         urls[result.url]['error'] = "BadRedirect"
-#                         urls[result.url]['success'] = False
-#                     except InvalidURL:
-#                         urls[result.url]['error'] = "InvalidURL"
-#                         urls[result.url]['success'] = False
-#                 else:
-#                     urls[result.url]['success'] = False
-#                     urls[result.url]['status_code'] = result.status_code
-#         else:
-#
-#             # print('there are {} urls at the end'.format(len(urls)))
-#             url_dict = {}
-#
-#             for key in urls:
-#                 original_url = urls[key]['original_url']
-#                 url_dict[original_url] = urls[key]
-#
-#             return url_dict
