b39725724c4bf4e29adb478612147d25fb07bfc4
Max Klein <isalix@gmail.com>
2020-06-03 15:19:59 -0700

    add retryable logic to twitter branch

--- utils/common.py
+++ utils/common.py
@@ -2,10 +2,14 @@ from enum import Enum
 from operator import eq, not_
 
 import simplejson as json
+import sqlalchemy.orm.session
+import warnings
 from collections import namedtuple
 import datetime
 
 NOT_FOUND_TWITTER_USER_STR = "<NOT_FOUND>"
+LOGS_DIR = str(pathlib.Path(BASE_DIR, "logs"))
+pathlib.Path(LOGS_DIR).mkdir(parents=True, exist_ok=True)
 
 EXPERIMENT_LANGUAGES = ['en', 'en-gb', 'en-GB']
 
@@ -18,11 +22,11 @@ class PageType(Enum):
     HOT = 4
 
 class ThingType(Enum):
-    SUBMISSION = 1
-    COMMENT = 2
-    SUBREDDIT = 3
-    USER = 4
-    STYLESHEET = 5
+	SUBMISSION = 1
+	COMMENT = 2
+	SUBREDDIT = 3
+	USER = 4
+	STYLESHEET = 5
 
 # not formalized...
 class TwitterUserState(Enum):
@@ -115,63 +119,78 @@ class EventWhen(Enum):
     BEFORE = 1
     AFTER = 2
 
+class RetryableDbSession(sqlalchemy.orm.session.Session):
+    @retryable(backoff=True)
+    def execute_retryable(self, clause, params=None, commit=True):
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", r"\(1062, \"Duplicate entry")
+            self.execute(clause, params)
+            if commit:
+                self.commit()
+
+    def insert_retryable(self, model, params, commit=True, ignore_dupes=True):
+        clause = model.__table__.insert()
+        if ignore_dupes:
+            clause = clause.prefix_with("IGNORE")
+        self.execute_retryable(clause, params, commit)
+
 class DbEngine:
-    def __init__(self, config_path):
-        self.config_path = config_path
+	def __init__(self, config_path):
+		self.config_path = config_path
+    
+	def new_session(self):
+		with open(self.config_path, "r") as config:
+		    DBCONFIG = json.loads(config.read())
 
-    def new_session(self):
-        with open(self.config_path, "r") as config:
-            DBCONFIG = json.loads(config.read())
-
-        from sqlalchemy import create_engine
-        from sqlalchemy.orm import sessionmaker
-        from app.models import Base
+		from sqlalchemy import create_engine
+		from sqlalchemy.orm import sessionmaker
+		from app.models import Base
         db_engine = create_engine("mysql://{user}:{password}@{host}/{database}?charset=utf8mb4".format(
-            host = DBCONFIG['host'],
-            user = DBCONFIG['user'],
-            password = DBCONFIG['password'],
+		    host = DBCONFIG['host'],
+		    user = DBCONFIG['user'],
+		    password = DBCONFIG['password'],
             database = DBCONFIG['database']), pool_recycle=3600, encoding='utf8')
 
-        Base.metadata.bind = db_engine
+		Base.metadata.bind = db_engine
         DBSession = sessionmaker(bind=db_engine)
-        db_session = DBSession()
-        return db_session
+		db_session = DBSession()
+		return db_session
 
 def _json_object_hook(dobj):
-    dobj['json_dict'] = dobj.copy()
-    X =  namedtuple('X', dobj.keys(), rename=True)
-    X.remove = lambda x: None
-    return(X(*dobj.values()))
+	dobj['json_dict'] = dobj.copy()
+	X =  namedtuple('X', dobj.keys(), rename=True)
+	X.remove = lambda x: None
+	return(X(*dobj.values()))
 
 def json2obj(data):
-    return json.loads(data, object_hook=_json_object_hook)
+	return json.loads(data, object_hook=_json_object_hook)
 
 
 class CommentNode:
-    def __init__(self, id, data, link_id = None, toplevel = False, parent=None):
-        self.id = id
-        self.children = list()
-        self.parent = parent
-        self.link_id = link_id
-        self.toplevel = toplevel
-        self.data = data
+	def __init__(self, id, data, link_id = None, toplevel = False, parent=None):
+		self.id = id
+		self.children = list()
+		self.parent = parent
+		self.link_id = link_id
+		self.toplevel = toplevel
+		self.data = data
 
-    def add_child(self, child):
-        self.children.append(child)
+	def add_child(self, child):
+		self.children.append(child)
 
-    def set_parent(self,parent):
-        self.parent = parent
+	def set_parent(self,parent):
+		self.parent = parent
 
-    def get_all_children(self):
-        all_children = self.children
-        for child in self.children:
-            all_children = all_children + child.get_all_children()
-        if(len(all_children)>0):
-            return all_children
-        else:
-            return []
+	def get_all_children(self):
+		all_children = self.children
+		for child in self.children:
+			all_children = all_children + child.get_all_children()
+		if(len(all_children)>0):
+			return all_children
+		else:
+			return []
 
-    def __str__(self):
+	def __str__(self):
         return str(self.id)
 
 
---
+++ utils/retry.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+
+import argparse
+import functools
+import os
+import random
+import subprocess
+import sys
+import time
+from pathlib import Path
+
+import app.cs_logger
+
+ENV = os.environ["CS_ENV"]
+BASE_DIR = str(Path(__file__).parents[1])
+_log = app.cs_logger.get_logger(ENV, BASE_DIR)
+
+RETRY = True
+RETRY_MAX_TIMES = 5
+RETRY_WAIT = 3
+BACKOFF = False
+BACKOFF_BASE = 5
+BACKOFF_MAX_EXP = 4
+BACKOFF_MAX_TIMES = 5
+
+def retryable(fn=None, retry=RETRY, backoff=BACKOFF, retry_wait=RETRY_WAIT,
+              retry_max_times=RETRY_MAX_TIMES, backoff_base=BACKOFF_BASE,
+              backoff_max_exp=BACKOFF_MAX_EXP,
+              backoff_max_times=BACKOFF_MAX_TIMES, _testing=False):
+    if fn is None:
+        return functools.partial(retryable, retry=retry, backoff=backoff,
+                                 retry_wait=retry_wait,
+                                 retry_max_times=retry_max_times,
+                                 backoff_base=backoff_base,
+                                 backoff_max_exp=backoff_max_exp,
+                                 backoff_max_times=backoff_max_times)
+
+    def _backoff_exps():
+        valid_exp = lambda i: i if i < backoff_max_exp else backoff_max_exp
+        yield from (valid_exp(i) for i in range(backoff_max_times))
+    retryable._backoff_exps = _backoff_exps
+
+    def _log_attempt(from_backoff, attempt_num, attempt_max, sleep_time):
+        attempt_type = "Backoff" if from_backoff else "Retry"
+        msg = "%s attempt %d of %d failed. Sleeping for %d seconds."
+        _log.info(msg, attempt_type, attempt_num, attempt_max, sleep_time)
+
+    def _wait():
+        if not retry and not backoff:
+            yield 0
+
+        if retry:
+            for i in range(retry_max_times):
+                yield retry_wait
+                _log_attempt(False, i+1, retry_max_times, retry_wait)
+                time.sleep(retry_wait if not _testing else 0)
+        
+        if backoff:
+            for i, exp in enumerate(_backoff_exps()):
+                wait = backoff_base + random.randrange(0, backoff_base**exp)
+                yield wait
+                _log_attempt(True, i+1, backoff_max_times, wait)
+                time.sleep(wait if not _testing else 0)
+
+    @functools.wraps(fn)
+    def _retry(*args, **kwargs):
+        _retry._retryable_last_exception = None
+        _retry._retryable_sleep_periods = []
+
+        for next_sleep_period in _wait():
+            _retry._retryable_sleep_periods.append(next_sleep_period)
+            try:
+                result = fn(*args, **kwargs)
+                _retry.retryable_last_exception = None
+                return result
+            except Exception as e:
+                _retry._retryable_last_exception = e
+                _log.exception("Exception encountered in a retryable function")
+
+        if _retry._retryable_last_exception:
+            raise _retry._retryable_last_exception
+
+    return _retry
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path",
+                        help="The path of the executable to retry on failure.")
+    parser.add_argument("arguments",
+                        nargs="*",
+                        help="Arguments to pass to the executable.")
+    parser.add_argument("--retry",
+                        action="store_true",
+                        default=RETRY,
+                        help="Whether retry loops are enabled.")
+    parser.add_argument("--retry-max-times",
+                        type=int,
+                        default=RETRY_MAX_TIMES,
+                        help="The maximum number of retry attempts.")
+    parser.add_argument("--retry-wait",
+                        type=int,
+                        default=RETRY_WAIT,
+                        help="The wait period between retries in seconds.")
+    parser.add_argument("--backoff",
+                        action="store_true",
+                        default=BACKOFF,
+                        help="Whether exponential backoff is enabled.")
+    parser.add_argument("--backoff-base",
+                        type=int,
+                        default=BACKOFF_BASE,
+                        help=("The base for exponential backoff, i.e. the " +
+                              "the minimum wait window size."))
+    parser.add_argument("--backoff-max-exp",
+                        type=int,
+                        default=BACKOFF_MAX_EXP,
+                        help=("The exponent for exponential backoff, i.e. " +
+                              "the maximum wait window size."))
+    parser.add_argument("--backoff-max-times",
+                        type=int,
+                        default=BACKOFF_MAX_TIMES,
+                        help="The maximum number of backoff attempts.")
+    
+    args = parser.parse_args()
+    return args
+
+def run_subprocess(**kwargs):
+    kwargs.setdefault("retry", RETRY)
+    kwargs.setdefault("retry_max_times", RETRY_MAX_TIMES)
+    kwargs.setdefault("retry_wait", RETRY_WAIT)
+    kwargs.setdefault("backoff", BACKOFF)
+    kwargs.setdefault("backoff_base", BACKOFF_BASE)
+    kwargs.setdefault("backoff_max_exp", BACKOFF_MAX_EXP)
+    kwargs.setdefault("backoff_max_times", BACKOFF_MAX_TIMES)
+
+    path = kwargs.pop("path")
+    arguments = kwargs.pop("arguments", [])
+
+    @retryable(**kwargs)
+    def _exec():
+        statement = [args.path] + args.arguments
+        completed = subprocess.run(statement)
+        return completed.returncode
+    
+    returncode = _exec()
+    sys.exit(returncode)
+
+if __name__ == "__main__":
+    try:
+        args = parse_args()
+        run_subprocess(**vars(args))
+    except KeyboardInterrupt:
+        pass
+
