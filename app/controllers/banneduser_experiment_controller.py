faf584061c1946d86e5817f4e57356bf66b7cf78
Dan Taeyoung <dan@dantaeyoung.com>
2024-07-25 16:11:27 -0400

    banneduser - finding eligible newcomers

--- app/controllers/banneduser_experiment_controller.py
+++ app/controllers/banneduser_experiment_controller.py
@@ -21,17 +21,21 @@ from app.controllers.messaging_controller import MessagingController
 from app.controllers.experiment_controller import *
 from collections import defaultdict
 
+### BANNED USER EXPERIMENT CONTROLLER
+
+
 ### DESCRIPTION OF THIS EXPERIMENT CONTROLLER
 # This experiment controller should:
-# 1. Observe modactions to identify new participants
-#   -- Criteria include: temporarily banned users.
+# 1. Observe modactions to identify and enroll new participants
 # 2. Randomly assign these users to receive different types of private messages
 # 3. TBD
 
 #### CALLBACK BEHAVIOR: enroll_new_participants
-## 1. Find banned users
-## 2. Check eligibility from ban status (temporary bans only)
-## 3. Check eligibility based on TBD
+## 1. Find banned users ("action": "banuser")
+## 2. Check eligibility from ban status (temporary bans only; no permabans) 
+##    -- Temporary ban: ("details": "1 days" or "30 days")
+##    -- Permanent ban: ("details": "permanent")
+
 
 #### REGULARLY SCHEDULED JOB BEHAVIOR (intervention): update_experiment
 ## 1....
@@ -65,8 +69,6 @@ class ModactionExperimentController(ExperimentController):
     """
 
     ## enroll_new_participants:
-    # Listen to callback and process new mod actions acquired.
-    # These will be used
 
     def enroll_new_participants(self, instance):
         if instance.fetched_subreddit_id != self.experiment_settings["subreddit_id"]:
@@ -84,14 +86,119 @@ class BanneduserExperimentController(ModactionExperimentController):
     ):
         super().__init__(experiment_name, db_session, r, log, required_keys)
 
-    ## enroll_new_participants:
-    # Listen to callback and process new mod actions acquired.
-    # Identify banned users in those mod actions, and start the
-    # process of determining if they are eligible to be enrolled
-    # in the study
-    ## Called after ModeratorController.archive_mod_action_page
+
+
+    def previously_enrolled_users(self):
+        previously_enrolled = defaultdict(list)
+        for et in self.db_session.query(Experiment).filter(and_(
+            ExperimentThing.experiment_id == self.experiment.id,
+            ExperimentThing.object_type == ThingType.USER.value,
+        )).order_by(ExperimentThing.created_at).all():
+            previously_enrolled[et.thing_id].append(et)
+
+        return previously_enrolled
+            
+
+
+
+
+
+
+
+
+
+
+    #### FIND ELIGIBLE NEWCOMERS
+
+    # Accepts a list of modactions
+    # and returns a list of users who have been newly temporarly banned
+    # ('newly' as in: we haven't noticed them before)
+
+    def find_eligible_newcomers(self, modactions):
+
+
+
+        ### LOGIC for finding temporary bans
+
+        def is_tempban(modaction):
+            return (modaction['action'] == 'banuser') and ("days" in modaction['details'])
+
+
+
+        ### LOGIC for finding previously enrolled participants
+
+        previously_enrolled_user_ids = self.previously_enrolled_users().keys()
+
+        def is_not_previously_enrolled(modaction):
+            return modaction['target_author'] not in previously_enrolled_user_ids
+
+
+
+        ### iterate over modactions and find eligible newcomers
+
+        eligible_newcomers = {}
+
+        for modaction in modactions:
+
+            if(is_tempban(modaction) and is_not_previously_enrolled(modaction)):
+
+                eligible_newcomers[modaction['target_author']] = modaction
+                # TODO: handle logic if the same user has multiple ban events 
+
+        return eligible_newcomers
+
+
+
+
+
+
+    def assign_randomized_conditions(self, new_tempbans):
+        self.log.info("###### Assigning randomized conditions")
+
+        self.db_session.execute("Lock Tables experiments WRITE, experiment_things WRITE")
+        try:
+            for newcomer in new_tempbans:
+                self.log.info(newcomer)
+
+                # WRITE STUFF
+
+        except(Exception) as e:
+           self.log.error("Error in BanneduserExperimentController::assign_randomized_conditions", extra=sys.exc_info()[0])
+           return []
+        finally:
+           self.db_session.execute("UNLOCK TABLES")
+
+        return
+
+
+
+
+    ################################################### 
+    ################################################### 
+    ################################################### 
+    ## ENROLL NEW PARTICIPANTS
+    ## Called from ModeratorController.archive_mod_action_page
 
     def enroll_new_participants(self, instance):
+
+
+
+        ### STEP 1. FIND ELIGIBLE NEWCOMERS
+
+        self.log.info("Experiment {0}: scanning modactions in subreddit {1} to look for temporary bans".format(self.experiment.name, self.experiment_settings['subreddit_id']))
+
+        eligible_newcomers = self.find_eligible_newcomers(instance.fetched_mod_actions)
+
+   
+        ### STEP 2. ASSIGN RANDOMIZED CONDITIONS
+        
+        self.log.info("Assigning randomized conditions to eligible newcomers")
+
+        self.assign_randomized_conditions(eligible_newcomers)
+
+
+
+       
         if instance.fetched_subreddit_id != self.experiment_settings["subreddit_id"]:
             return
         self.log.info(
