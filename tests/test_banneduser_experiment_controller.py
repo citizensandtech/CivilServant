535ce9692371884a96e5178049b8bae11ba9f24b
Dan Taeyoung <dan@dantaeyoung.com>
2025-03-31 13:35:48 -0400

    test_get_accounts_needing_second_banover_interventions

--- tests/test_banneduser_experiment_controller.py
+++ tests/test_banneduser_experiment_controller.py
@@ -566,7 +566,6 @@ class TestPrivateMethods:
 
 
         for user in test_users:
-            print(user)
             et = ExperimentThing(
                 id=user["thing_id"],
                 thing_id=user["thing_id"],
@@ -579,6 +578,61 @@ class TestPrivateMethods:
         experiment_controller.db_session.commit()
 
         users = experiment_controller._get_accounts_needing_first_banstart_interventions()
+
+        assert len(users) == want
+
+
+    @pytest.mark.parametrize(
+        "test_users,want",
+        [
+            (
+                [{
+                    "thing_id": "user_pending",
+                    "query_index": BannedUserQueryIndex.SECOND_BANOVER_PENDING,
+                }],
+                1,
+            ),            
+            (
+                [{
+                    "thing_id": "user_pending",
+                    "query_index": BannedUserQueryIndex.FIRST_BANSTART_PENDING,
+                }],
+                0,
+            ),            
+            (
+                [{
+                    "thing_id": "user_complete",
+                    "query_index": BannedUserQueryIndex.FIRST_BANSTART_COMPLETE,
+                },
+                {
+                    "thing_id": "user_pending",
+                    "query_index": BannedUserQueryIndex.SECOND_BANOVER_PENDING,
+                }],
+                1,
+            ),
+        ],
+    )
+    def test_get_accounts_needing_second_banover_interventions(
+        self, test_users, want, helpers, experiment_controller, mod_controller
+    ):
+
+        users = experiment_controller._get_accounts_needing_second_banover_interventions()
+        assert users == []
+
+
+        for user in test_users:
+            et = ExperimentThing(
+                id=user["thing_id"],
+                thing_id=user["thing_id"],
+                experiment_id=experiment_controller.experiment.id,
+                object_type=ThingType.USER.value,
+                query_index=user["query_index"],
+                metadata_json=json.dumps({"ban_type": "temporary"}),
+            )
+            experiment_controller.db_session.add(et)
+        experiment_controller.db_session.commit()
+
+        users = experiment_controller._get_accounts_needing_second_banover_interventions()
 
         assert len(users) == want
 
